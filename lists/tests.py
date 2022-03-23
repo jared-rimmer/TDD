b525a75c8c115ec8c26a4b2a790b806e3637b205
jared.rimmer <jared.rimmer1+github@gmail.com>
2022-03-23 20:45:21 +0000

    Add more test cases to functional tests

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from urllib import response
 from django.test import TestCase
 
 
@@ -9,3 +10,7 @@ class HomePageTest(TestCase):
         
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item' })
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
