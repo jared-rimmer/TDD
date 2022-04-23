23bce29d76317ba2e9acbf98f6894a6a38908f52
jared.rimmer <jared.rimmer1+github@gmail.com>
2022-04-23 16:23:25 +0100

    Add new form to homepage and simplify tests. NB breaks stuff

--- lists/views.py
+++ lists/views.py
@@ -2,11 +2,12 @@ from curses.ascii import HT
 from typing import List
 from django.forms import ValidationError
 from django.shortcuts import redirect, render
+from lists.forms import ItemForm
 from lists.models import Item, List
 
 # Create your views here.
 def home_page(request):
-    return render(request, "home.html")
+    return render(request, "home.html", {"form": ItemForm()})
 
 
 def view_list(request, list_id):
